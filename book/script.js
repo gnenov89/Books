f4bc37f8216396152416e49389eb6781ea049177
Georgi Nenov <georgi.nenov89@gmail.com>
2018-01-04 09:07:25 -0500

    adding items to the beggining and the end of the list

--- book/script.js
+++ book/script.js
@@ -1,4 +1,41 @@
 
+
+
+// 16.Example DOM
+
+// ADDING ITEMS TO START AND END OF LIST
+var list = document.getElementsByTagName('ul')[0];                 // Get the <ul> element
+
+// ADD NEW ITEM TO END OF LIST
+var newItemLast = document.createElement('li');                    // Create element
+var newTextLast = document.createTextNode('cream');                // Create text node
+newItemLast.appendChild(newTextLast);                              // Add text node to element
+list.appendChild(newItemLast);                                     // Add element end of list
+
+// ADD NEW ITEM START OF LIST
+var newItemFirst = document.createElement('li');                   // Create element
+var newTextFirst = document.createTextNode('kompot');                // Create text node
+newItemFirst.appendChild(newTextFirst);                            // Add text node to element
+list.insertBefore(newItemFirst, list.firstChild);                  // Add element to list
+
+
+
+
+
+// 15.Removing attributes 
+// var firstItem = document.getElementById('one'); // Get the first item
+// if (firstItem.hasAttribute('class')) {          // If it has a class attribute
+//   firstItem.removeAttribute('class');           // Remove its class attribute
+// }
+
+
+
+
+
+
+
+
+
 // 14. Change attributes
 
 
