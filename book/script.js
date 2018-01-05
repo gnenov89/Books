ba502d4f7c65931eaec6986cb96e6bf3483f62c5
Georgi Nenov <georgi.nenov89@gmail.com>
2018-01-05 14:45:01 -0500

    mutation events

--- book/script.js
+++ book/script.js
@@ -1,5 +1,27 @@
 // **********************CHAPTER 6- EVENTS ***************************************
+// 12.Using Mutation events
+var elList, addLink, newEl, newText, counter, listItems; // Declare variables
 
+elList  = document.getElementById('list');               // Get list
+addLink = document.querySelector('a');                   // Get add item button
+counter = document.getElementById('counter');            // Get item counter
+
+function addItem(e) {                                    // Declare function
+  e.preventDefault();                                    // Prevent link action
+  newEl = document.createElement('li');                  // New <li> element
+  newText = document.createTextNode('New list item');    // New text node
+  newEl.appendChild(newText);                            // Add text to <li>
+  elList.appendChild(newEl);                             // Add <li> to list
+}
+
+function updateCount() {                                 // Declare function
+  listItems = elList.getElementsByTagName('li').length;  // Get total of <li>s
+  counter.innerHTML = listItems;                         // Update counter
+}
+
+addLink.addEventListener('click', addItem, false);       // Click on button
+elList.addEventListener('DOMNodeInserted', updateCount, false); // DOM updated
+   
 // 11.Form
 // var elForm, elSelectPackage, elPackageHint, elTerms, elTermsHint; // Declare variables
 // elForm          = document.getElementById('formSignup');          // Store elements
