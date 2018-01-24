6a74cd7cfbe1cea49f126a7ea680e24e5bd4d023
gnenov89 <georgi.nenov89@gmail.com>
2018-01-24 14:02:25 -0500

    using localstorage

--- book/script.js
+++ book/script.js
@@ -1,4 +1,30 @@
 
+
+
+
+// <!-- CHAPTER 9 API'S -->
+
+// <!-- 1.Working with storage -->
+
+// This example has been updated to use Modernizr - please note the use of localstorage in lowercase
+if (Modernizr.localstorage) {
+
+  var txtUsername = document.getElementById('username'); // Get form elements
+  var txtAnswer = document.getElementById('answer');
+
+  txtUsername.value = localStorage.getItem('username');  // Elements populated
+  txtAnswer.value = localStorage.getItem('answer');      // by localStorage data
+
+  txtUsername.addEventListener('input', function () {    // Data saved on keyup
+    localStorage.setItem('username', txtUsername.value);
+  }, false);
+
+  txtAnswer.addEventListener('input', function () {      // Data saved on keyup
+    localStorage.setItem('answer', txtAnswer.value);
+  }, false);
+}
+
+
 // <!-- Chapter 8 AJAX/JSON -->
 
 
