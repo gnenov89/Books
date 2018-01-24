7405d826f931a1ba00bb842994214d0fc8ec0c8e
gnenov89 <georgi.nenov89@gmail.com>
2018-01-24 09:08:24 -0500

    ajax call GIPHY API

--- book/AJAX/Final Excersie/final.js
+++ book/AJAX/Final Excersie/final.js
@@ -20,6 +20,44 @@ $(document).ready(function(){
           }
       }
 
-      $()
+      $(document).on("click",".amnimal-button", function(){
+          $("#animals").empty();
+          $("#animal-button").removeClass("active");
+          $(this).addClass("active");
+
+          var typ =$(this).attr("data-type");
+            var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=dc6zaTOxFJmzC&limit=10";
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+              })
+              .done(function(response) {
+                var results = response.data;
+          
+                for (var i = 0; i < results.length; i++) {
+                  var animalDiv = $("<div class=\"animal-item\">");
+          
+                  var rating = results[i].rating;
+          
+                  var p = $("<p>").text("Rating: " + rating);
+          
+                  var animated = results[i].images.fixed_height.url;
+                  var still = results[i].images.fixed_height_still.url;
+          
+                  var animalImage = $("<img>");
+                  animalImage.attr("src", still);
+                  animalImage.attr("data-still", still);
+                  animalImage.attr("data-animate", animated);
+                  animalImage.attr("data-state", "still");
+                  animalImage.addClass("animal-image");
+          
+                  animalDiv.append(p);
+                  animalDiv.append(animalImage);
+          
+                  $("#animals").append(animalDiv);
+                }
+              });
+            });
+    
 
 })
