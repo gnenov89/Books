79fb1f4d304b5a9a9252f9078876060f18dff070
gnenov89 <georgi.nenov89@gmail.com>
2018-01-26 10:09:17 -0500

    console methods

--- book/script.js
+++ book/script.js
@@ -3,33 +3,54 @@
 // CHAPTER 10. DEBUGING 
 
 
+//5. Console Methods
+console.info('And we\'re off...');                    // Info: script running
 
-
-
-
-console.log('And we\'re off...');               // Indicates script is running
 var $form, width, height, area;
 $form = $('#calculator');
 
-$('form input[type="text"]').on('blur', function() { // When input loses focus
-  console.log('You entered ', this.value );          // Write value to console
+$('form input[type="text"]').on('blur', function() {  // On blur event
+  console.warn('You entered ', this.value);           // Warn: what was entered
 });
 
-$('#calculator').on('submit', function(e) {     // When the user clicks submit
-  e.preventDefault();                           // Prevent the form submitting
-  console.log('Clicked submit...');             // Indicate button was clicked
+$('#calculator').on('submit', function(e) {           // When form is submitted
+  e.preventDefault();
 
   width = $('#width').val();
-  console.log('Width ' + width);                // Write width to console
-
   height = $('#height').val();
-  console.log('Height ', height);               // Write height to console
 
   area = width * height;
-  console.log(area);                            // Write area to console
+  console.error(area);                                // Error: show area
 
-  $form.append('<p>' + area + '</p>')
-});
+  $form.append('<p class="result">' + area + '</p>');
+}); 
+
+
+
+
+// console.log('And we\'re off...');               // Indicates script is running
+// var $form, width, height, area;
+// $form = $('#calculator');
+
+// $('form input[type="text"]').on('blur', function() { // When input loses focus
+//   console.log('You entered ', this.value );          // Write value to console
+// });
+
+// $('#calculator').on('submit', function(e) {     // When the user clicks submit
+//   e.preventDefault();                           // Prevent the form submitting
+//   console.log('Clicked submit...');             // Indicate button was clicked
+
+//   width = $('#width').val();
+//   console.log('Width ' + width);                // Write width to console
+
+//   height = $('#height').val();
+//   console.log('Height ', height);               // Write height to console
+
+//   area = width * height;
+//   console.log(area);                            // Write area to console
+
+//   $form.append('<p>' + area + '</p>')
+// });
 // console.log('And we\'re off');
 
 // var $form;
